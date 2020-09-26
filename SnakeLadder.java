a27d151dd623723b328f68a73d91e721dd107792
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 16:25:16 +0530

    Repeat till 100 feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,7 @@
 public class SnakeLadder{
 	
 	public static int position = 0;
-	//public static int die = 0;
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game played with single player at start position 0");
