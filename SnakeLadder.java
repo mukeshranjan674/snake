2d2aa98cb275cd25024dd6837d511dd7c32f04fd
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 16:14:48 +0530

    Single player started at position 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,4 +1,5 @@
 public class SnakeLadder{
+	public static int position = 0;
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game played with single player at start position 0");
 	}
