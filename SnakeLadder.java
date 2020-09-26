d6ba8e372b0eaa4d154f79711cbef90824969ac9
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 14:31:16 +0530

    uc3 feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,33 @@
 public class SnakeLadder{
+	
+	public static int position = 0;
+	
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game played with single player at start position 0");
-		int n = rollDie();
+		int check = checkOption();
 	}
 	
 	public static int rollDie() {
 		int check = (int)(Math.floor(Math.random() * 10) % 7);
 		System.out.println("Die Value is : "+check);
+		return check;
+	}
+	public static int checkOption() {
+		int check = (int)(Math.floor(Math.random() * 10) % 3);
+		switch(check) {
+			case 1:
+				position = position + rollDie();
+				System.out.println("[Option : Ladder]" + " Player moves to position " + position);
+				break;
+				
+			case 2:
+				position = position - rollDie();
+				System.out.println("[Option : Snake]" + " Player moves to position " + position);
+				break;
+			default:
+				System.out.println("[Option : No Play]" + " Player remains at position " + position);
+				
+		}
 		return check;
 	}
 }
