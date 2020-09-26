e16660e2c062d8a37acdeceb01735a6bfab0a232
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 15:14:07 +0530

    uc4 feture added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,33 +1,45 @@
 public class SnakeLadder{
 	
 	public static int position = 0;
+	//public static int die = 0;
 	
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game played with single player at start position 0");
-		int check = checkOption();
+		while(position < 100) {
+			int check = checkOption();
+			
+		}
 	}
 	
 	public static int rollDie() {
-		int check = (int)(Math.floor(Math.random() * 10) % 7);
-		System.out.println("Die Value is : "+check);
-		return check;
+		int die = 0;
+		while(die == 0) {
+			die = (int)(Math.floor(Math.random() * 10) % 7);
+		}
+		System.out.print("Die Value is : "+die);
+		return die;
 	}
 	public static int checkOption() {
 		int check = (int)(Math.floor(Math.random() * 10) % 3);
+		int value = rollDie();
 		switch(check) {
 			case 1:
-				position = position + rollDie();
-				System.out.println("[Option : Ladder]" + " Player moves to position " + position);
+				position = position + value;
+				System.out.println(" [Option : Ladder]\n" + " Player moves to position " + position + "\n");
 				break;
 				
 			case 2:
-				position = position - rollDie();
-				System.out.println("[Option : Snake]" + " Player moves to position " + position);
+				position = position - value;
+				if (position < 0)
+					position = 0;
+				System.out.println(" [Option : Snake]\n" + " Player moves to position " + position + "\n");
 				break;
 			default:
-				System.out.println("[Option : No Play]" + " Player remains at position " + position);
+				System.out.println(" [Option : No Play]\n" + " Player remains at position " + position + "\n");
+				
 				
 		}
+		
 		return check;
 	}
 }
