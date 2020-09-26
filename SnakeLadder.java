653c8602e3f4f5348db732b24708c360d81abbfe
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 14:15:54 +0530

    uc2 feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,12 @@
 public class SnakeLadder{
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game played with single player at start position 0");
+		int n = rollDie();
+	}
+	
+	public static int rollDie() {
+		int check = (int)(Math.floor(Math.random() * 10) % 7);
+		System.out.println("Die Value is : "+check);
+		return check;
 	}
 }
