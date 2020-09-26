07be00ae9478a945cf7bf8bf9b605f6ac92b6595
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 16:23:02 +0530

    check for option feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,9 +8,12 @@ public class SnakeLadder{
 	}
 	
 	public static int rollDie() {
-		int check = (int)(Math.floor(Math.random() * 10) % 7);
-		System.out.println("Die Value is : "+check);
-		return check;
+		int die = 0;
+		while(die == 0) {
+			die = (int)(Math.floor(Math.random() * 10) % 7);
+		}
+		System.out.println("Die Value is : "+die);
+		return die;
 	}
 	public static int checkOption() {
 		int check = (int)(Math.floor(Math.random() * 10) % 3);
