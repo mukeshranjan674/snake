2416949ce7f66c2507620c357390f847beb2d0c1
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 16:07:47 +0530

    uc7 feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,15 +1,10 @@
 public class SnakeLadder{
 	
-	public static int position = 0;
+	public static int position_a = 0 , position_b = 0 , position = 0;
 	public static int die_count = 0;
 	
 	public static void main(String[] args) {
-		System.out.println("Snake and Ladder game played with single player at start position 0");
-		while(position < 100) {
-			int check = checkOption();
-			
-		}
-		System.out.println("Number of times die was rolled : " + die_count);
+		twoPlayer();
 	}
 	
 	public static int rollDie() {
@@ -39,11 +34,40 @@ public class SnakeLadder{
 				System.out.println(" [Option : Snake]\n" + " Player moves to position " + position + "\n");
 				break;
 			default:
-				System.out.println(" [Option : No Play]\n" + " Player remains at position " + position + "\n");
-				
-				
+				System.out.println(" [Option : No Play]\n" + " Player remains at position " + position + "\n");	
 		}
 		
 		return check;
+	}
+	
+	public static void twoPlayer() {
+		int term = 2; int check = 0;
+		System.out.println("Snake and Ladder game played with two players starting at position 0");
+		while(position_a < 100 && position_b < 100) {
+			int turn = term % 2;
+			if (turn == 0) {
+				term++;
+				do {
+					System.out.println("Player A at " + position_a);
+					position = position_a;
+					check = checkOption();
+					position_a = position;
+				}while(check == 1 && position_a != 100);
+			}
+			
+			else {
+				term++;
+				do {
+					System.out.println("Player B at " + position_b);
+					position = position_b;
+					check = checkOption();
+					position_b = position;
+				}while(check == 1 && position_b != 100);
+			}
+		}
+		if (position_a == 100)
+			System.out.println("Player A won !!!");
+		else
+			System.out.println("Player B won !!!");
 	}
 }
